4f51c6f5f5a7d169c8bad758f15e78675d7f3bd3
Francesco <francesco.donini@iit.cnr.it>
2021-03-13 17:58:22 +0100

    Added support to compile and let developers use any text editor of their choice and compile, run or package their project using make

---
+++ properties.mk
@@ -0,0 +1,4 @@
+DEVICE = edge_520
+SDK_HOME = /Users/francesco/Library/Developer/connectiq-sdk-mac-3.1.8-2020-05-01-5a72d0ab2
+DEPLOY = /Volumes/GARMIN/GARMIN/APPS/
+PRIVATE_KEY = /Users/francesco/.ssh/developer_key-use_to_Sign.der
